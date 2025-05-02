eb669b8f01f49606c58bdcc6f31033eb2a88de18
dustinburda <dustin99wii@gmail.com>
2025-05-01 23:57:31 -0400

    boilerplate

--- SlackClone/backend/index.js
+++ SlackClone/backend/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+require('dotenv').config()
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('../frontend'))
+
+app.get('/', (req, res) => {
+    res.set({
+        "content-type": "text/plain"
+    })
+    res.send("<h1> Hello World! </h1>")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
