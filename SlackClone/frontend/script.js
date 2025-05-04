1baee08dead4e7734ae9458533eb384ff84004c3
dustinburda <dustin99wii@gmail.com>
2025-05-03 22:09:43 -0400

    login card

---
+++ SlackClone/frontend/script.js
@@ -0,0 +1,7 @@
+let loginPage = document.querySelector(".login-page")
+window.console.log(loginPage)
+
+let welcomePage = document.querySelector(".welcome-page")
+window.console.log(welcomePage)
+
+loginPage.classList.remove("hidden")
