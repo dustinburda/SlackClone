0406410e6faa0006947f949edb23d5b096c6677e
dustinburda <dustin99wii@gmail.com>
2025-05-04 16:45:10 -0400

    More login page, some login logic

--- SlackClone/backend/index.js
+++ SlackClone/backend/index.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require("cors")
 
 const app = express();
+app.use(express.static('../frontend'))
+app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../frontend'))
+const credentials = {
+    "dustin": "1234"
+};
 
 app.get('/', (req, res) => {
     res.set({
@@ -15,6 +21,15 @@ app.get('/', (req, res) => {
     res.send("<h1> Hello World! </h1>");
 })
 
+app.post('/api/login/', (req, res) => {
+    
+    const {username, password} = req.body;
+
+    
+
+    console.log(username, password)
+    res.status(500).send("Logging In...")
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
