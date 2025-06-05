fb9ff674e3c644ad649e0e6bb1f2c845d4cfcefc
dustinburda <dustin99wii@gmail.com>
2025-06-05 00:00:04 -0400

    .

---
+++ backend/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken (req, res, next) {
+    console.log(req.headers);
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null)
+        return res.status(401).send('No token provided');
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err)
+            return res.status(403).send('Token not valid');
+
+        req.user = user;
+        next();
+    })
+}
+
+module.exports = { authenticateToken };
--- backend/index.js
+++ backend/index.js
@@ -1,61 +1,99 @@
-require('dotenv').config()
+require('dotenv').config({
+    path: "./process.env"
+})
 
 const express = require('express');
 const cors = require("cors")
 const bycrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+
+const { authenticateToken} = require("./auth.js");
 
 const app = express();
 app.use(express.static('../frontend'))
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const credentials = {
 };
+
+
+
+posts = [
+    {
+        username: "Jim",
+        title: "post1"
+    },
+    {
+        username: "Joe",
+        title: "post2"
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.set({
+        'Content-Type': 'application/json',
+    })
+    console.log(req.user.name);
+    return res.send(JSON.stringify(posts.filter( post => post.username === req.user.name )))
+})
+
 
 app.get('/', (req, res) => {
     res.set({
         "content-type": "text/plain"
     });
-    res.send("<h1> Hello World! </h1>");
+    return res.send("<h1> Hello World! </h1>");
 })
 
 app.post('/api/signup/', async (req, res) => {
     const {username, password} = req.body;
+
+    if (credentials.hasOwnProperty(username))
+        return res.status(400).send("Username already exists!");
 
     try {
         const salt = await bycrypt.genSalt();
         const hashedPassword = await bycrypt.hash(password, salt);
 
         credentials[username] = hashedPassword;
-        res.status(201).send();
+        return res.status(201).send("Successfully signed up!");
 
     } catch (e) {
-        res.status(500).send();
+        return res.status(500).send("Failed to sign up!");
     }
 })
 
 app.post('/api/login/', async (req, res) => {
-    
+    console.log("Logging in...")
     const {username, password} = req.body;
 
     try {
         if (!(username in credentials))
-            res.status(404).send();
+            return res.status(404).send();
 
         validPassword = await bycrypt.compare(password, credentials[username]);
 
-        if (validPassword)
-            res.status(200).send();
+        if (validPassword) {
+            const user = { name: username };
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            return res.status(200).send(JSON.stringify(accessToken));
+        }
         else
-            res.status(404).send();
+            return res.status(404).send();
 
     } catch (e) {
-        res.status(500).send();
+        return res.status(500).send();
     }
 })
 
+app.post('/api/logout', async (req, res) => {
+    const {username} = req.body;
+
+    
+})
 
 
 app.listen(PORT, () => {
